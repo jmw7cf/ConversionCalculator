c43d6a5f7474eb1d288434c41924ad069498a503
Julia Wopata <julia.wopata@gmail.com>
2019-04-12 22:48:05 -0500

    Equal heights

--- ConversionCalculator/ConversionCalculator/ConversionCalculatorViewController.swift
+++ ConversionCalculator/ConversionCalculator/ConversionCalculatorViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversionCalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
